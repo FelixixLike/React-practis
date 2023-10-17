55ec423eecc2f8d932223fc15fae142b790ce25b
Felixix L_X <a.felipemg16@gmail.com>
2023-10-17 16:16:48 -0500

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+export function App() {
+  return (
+    <div className="bg-black text-white rounded-md text-center">
+      <h1 className="text-xl font-bold capitalize  ">Hola mundo 😊</h1>
+      <p className="text-white px-2 py-2" >Probando tailwinds 😎</p>
+    </div>
+      
+  )
+}
+
+
